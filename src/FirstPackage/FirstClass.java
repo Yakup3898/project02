a9f95534c9665b34fbca079e66d9315c385fadb9
Yakup Ozcan <yakupozc98@gmail.com>
2020-11-08 22:12:07 +0600

    third commit from local master

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -6,6 +6,7 @@ public class FirstClass {
 		
 		System.out.println("First commit from local master branch");
 		System.out.println("secomd commit from local master");
+		System.out.println("third commit from local master");
 
 	}
 
